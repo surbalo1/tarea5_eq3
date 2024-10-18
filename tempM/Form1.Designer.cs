84859204fa5b6113372f6cfa09110f60340a0bcd
surbalo1 <surbalo1@gmail.com>
2024-10-17 22:46:17 -0600

    interfaz

---
+++ tempM/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace tempM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pBof = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_a1 = new System.Windows.Forms.Label();
+            this.lbl_a2 = new System.Windows.Forms.Label();
+            this.lbl_a3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.comboBoxBaud = new System.Windows.Forms.ComboBox();
+            this.comboBoxPort = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_temporada = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pBof)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pBof
+            // 
+            this.pBof.Image = global::tempM.Properties.Resources.apagado;
+            this.pBof.Location = new System.Drawing.Point(456, 36);
+            this.pBof.Name = "pBof";
+            this.pBof.Size = new System.Drawing.Size(87, 64);
+            this.pBof.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pBof.TabIndex = 0;
+            this.pBof.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "apagado.jpg");
+            this.imageList1.Images.SetKeyName(1, "encendido.jpg");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Aula 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(204, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Aula 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(395, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Aula 3";
+            // 
+            // lbl_a1
+            // 
+            this.lbl_a1.AutoSize = true;
+            this.lbl_a1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_a1.Location = new System.Drawing.Point(87, 144);
+            this.lbl_a1.Name = "lbl_a1";
+            this.lbl_a1.Size = new System.Drawing.Size(31, 24);
+            this.lbl_a1.TabIndex = 4;
+            this.lbl_a1.Text = "---";
+            this.lbl_a1.Click += new System.EventHandler(this.lbl_a1_Click);
+            // 
+            // lbl_a2
+            // 
+            this.lbl_a2.AutoSize = true;
+            this.lbl_a2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_a2.Location = new System.Drawing.Point(279, 144);
+            this.lbl_a2.Name = "lbl_a2";
+            this.lbl_a2.Size = new System.Drawing.Size(31, 24);
+            this.lbl_a2.TabIndex = 5;
+            this.lbl_a2.Text = "---";
+            // 
+            // lbl_a3
+            // 
+            this.lbl_a3.AutoSize = true;
+            this.lbl_a3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_a3.Location = new System.Drawing.Point(470, 144);
+            this.lbl_a3.Name = "lbl_a3";
+            this.lbl_a3.Size = new System.Drawing.Size(31, 24);
+            this.lbl_a3.TabIndex = 6;
+            this.lbl_a3.Text = "---";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonClose);
+            this.groupBox1.Controls.Add(this.btnOpen);
+            this.groupBox1.Controls.Add(this.comboBoxBaud);
+            this.groupBox1.Controls.Add(this.comboBoxPort);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(16, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(227, 114);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "COM PORT";
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClose.Location = new System.Drawing.Point(127, 88);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(94, 20);
+            this.buttonClose.TabIndex = 8;
+            this.buttonClose.Text = "CLOSE";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.Location = new System.Drawing.Point(8, 88);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(94, 20);
+            this.btnOpen.TabIndex = 8;
+            this.btnOpen.Text = "OPEN";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // comboBoxBaud
+            // 
+            this.comboBoxBaud.FormattingEnabled = true;
+            this.comboBoxBaud.Items.AddRange(new object[] {
+            "9600",
+            "38400",
+            "57600",
+            "115200"});
+            this.comboBoxBaud.Location = new System.Drawing.Point(133, 55);
+            this.comboBoxBaud.Name = "comboBoxBaud";
+            this.comboBoxBaud.Size = new System.Drawing.Size(88, 21);
+            this.comboBoxBaud.TabIndex = 8;
+            // 
+            // comboBoxPort
+            // 
+            this.comboBoxPort.FormattingEnabled = true;
+            this.comboBoxPort.Location = new System.Drawing.Point(133, 24);
+            this.comboBoxPort.Name = "comboBoxPort";
+            this.comboBoxPort.Size = new System.Drawing.Size(88, 21);
+            this.comboBoxPort.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(33, 56);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "BAUD RATE";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(37, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "COM PORT:";
+            // 
+            // lbl_temporada
+            // 
+            this.lbl_temporada.AutoSize = true;
+            this.lbl_temporada.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_temporada.Location = new System.Drawing.Point(249, 65);
+            this.lbl_temporada.Name = "lbl_temporada";
+            this.lbl_temporada.Size = new System.Drawing.Size(75, 19);
+            this.lbl_temporada.TabIndex = 8;
+            this.lbl_temporada.Text = "VERANO";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(249, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(116, 19);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "TEMPORADA: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(555, 193);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbl_temporada);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbl_a3);
+            this.Controls.Add(this.lbl_a2);
+            this.Controls.Add(this.lbl_a1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pBof);
+            this.Name = "Form1";
+            this.Text = "temperature monitoring";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pBof)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pBof;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_a1;
+        private System.Windows.Forms.Label lbl_a2;
+        private System.Windows.Forms.Label lbl_a3;
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxBaud;
+        private System.Windows.Forms.ComboBox comboBoxPort;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label lbl_temporada;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ tempM/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+
+namespace tempM
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl_a1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pBof.Image = tempM.Properties.Resources.apagado;
+            try
+            {
+
+                string[] portlist = SerialPort.GetPortNames();
+                comboBoxPort.Items.AddRange(portlist);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = comboBoxPort.Text;
+                serialPort1.BaudRate = Convert.ToInt32(comboBoxBaud.Text);
+                serialPort1.Open();
+                timer1.Enabled = true;
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                timer1.Enabled = false;
+                serialPort1.Close();
+                lbl_a1.Text = "---";
+                lbl_a2.Text = "---";
+                lbl_a3.Text = "---";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        char[] delimitador = { ',' };
+        string[] temps;
+        string entrada;
+        string estacion;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            serialPort1.Write("l");
+
+            entrada = serialPort1.ReadLine();
+            temps = entrada.Split(delimitador);
+ 
+            if (temps[3] == "0")
+            {
+                pBof.Image = tempM.Properties.Resources.apagado;
+            }
+            else if (temps[3] == "1") {
+                pBof.Image = tempM.Properties.Resources.encendido;
+            }
+
+            if (temps[0] == "0.0" || temps[0] == "40.0")
+                 lbl_a1.Text = "---";
+            else
+                lbl_a1.Text = temps[0] + "°C";
+
+            if (temps[1] == "0.0" || temps[1] == "40.0")
+                lbl_a2.Text = "---";
+            else
+                lbl_a2.Text = temps[1] + "°C";
+
+            if (temps[2] == "0.0" || temps[2] == "40.0")
+                lbl_a3.Text = "---";
+            else
+                 lbl_a3.Text = temps[2] + "°C";
+
+            estacion = temps[4];
+
+            if (estacion == "1") lbl_temporada.Text = "INVIERNO";
+            if (estacion == "2") lbl_temporada.Text = "OTOÑO-PRIMAVERA";
+            if (estacion == "3") lbl_temporada.Text = "VERANO";
+
+        }
+    }
+}
